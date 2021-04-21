5092d7997c735aa45c1fdf8a48f847f9747d2698
crsmn <universitario180@hotmail.com>
2021-04-21 11:24:06 -0500

    s

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ const port = process.env.PORT || 4000;
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: false, limit: "20mb" }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 mongoose.connect(
   "mongodb+srv://javier:123@cluster0.cdkll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true }
